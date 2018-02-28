fd293cbf4928da0862a8c9280a9367a3e0a0e547
Trey Schneider <trey@codeschneider.com>
2018-02-27 23:09:18 -0500

    added fast-csv, file size | file type | empty file validation --> for global tag upload

--- api/controllers/TagController.js
+++ api/controllers/TagController.js
@@ -1,5 +1,9 @@
 const UIDGenerator = require('uid-generator');
 const uidgen = new UIDGenerator();
+const csv = require("fast-csv");
+const path = require("path");
+const fs = require('fs');
+
 
 module.exports = {
 
@@ -32,108 +36,94 @@ module.exports = {
   },
 
   uploadLoincDocumentOntologyCSV: function(req,res) {
-    var fileFormat = req.param('fileFormat');
-    var tagLookUp = {};
-    var subjectMatterDomainArr = [];
 
-    req.file('tagFile').upload({
-      adapter: require('skipper-csv'),
-      csvOptions: {delimiter: ',', columns: true},
-      rowHandler: function(row, fd){
-        if (fileFormat !== 'simple') {
-          var groupId = row['groupId'];
-          var attribute = row['attribute'];
-          var attributeValue = row['attributeValue'];
+    req.file('tagFile').upload({ maxBytes: 1000000}, function(err, uploadedFiles) {
+      if (err) {
+        sails.log.error(err);
+        return res.send(500, { message: 'Problem uploading file.' } );
+      }
 
-          if (!tagLookUp[groupId]) {
-            tagLookUp[groupId] = {}
-          }
+      if (uploadedFiles.length === 0) {
+        return res.send(400, { message: 'No file sent.' } );
+      }
 
-          var obj = tagLookUp[groupId];
+      if (uploadedFiles[0].type !== 'text/csv') {
+        return res.send(400, { message: 'File must be csv format. ' } );
+      }
 
-          obj['groupId'] = groupId;
-          if (attribute == 'Document.TypeOfService') {
-            obj['documentTypeOfService'] = attributeValue;
-          } else if (attribute == 'Document.Setting') {
-            obj['documentSetting'] = attributeValue;
-          } else if (attribute == 'Document.Role') {
-            obj['documentRole'] = attributeValue;
-          } else if (attribute == 'Document.Kind') {
-            obj['documentKind'] = attributeValue;
-          } else if (attribute == 'Document.SubjectMatterDomain') {
-            obj['documentSubjectMatterDomain'] = attributeValue;
-            obj['normDSMD'] = attributeValue.toLowerCase();
-          }
-        } else {
-          var tagValue = row['tag'];
+      var tagLookUp = {};
+
+      csv
+        .fromPath(uploadedFiles[0].fd, {
+          headers: ["tag"],
+          ignoreEmpty: true
+        })
+        .validate(function(data){
+          return data.tag !== 'tag';
+         })
+        .on('data', function(data) {
+          var tag = data.tag;
           var uid = uidgen.generateSync();
           tagLookUp[uid] = {
-            documentSubjectMatterDomain: tagValue,
-            normDSMD: tagValue.toLowerCase()
+            documentSubjectMatterDomain: tag,
+            normDSMD: tag.toLowerCase()
           };
-        }
-      },
-      maxBytes: 10000000
-    },function whenDone(err, uploadedFiles) {
-      if (err) {
-        return res.serverError(err);
-      }
-      if (uploadedFiles.length === 0){
-        return res.badRequest('No file was uploaded');
-      }
-      console.log('tagLookUp: ',tagLookUp);
-      var tagsArr = [];
-      _.forEach(tagLookUp, function(value, key) {
-        value['origin'] = 'Global';
-        if (value.documentSubjectMatterDomain) {
-          tagsArr.push(value);
-        }
-      });
+        })
+        .on('end', function() {
 
-      async.map(tagsArr, function(tag, mapCb) {
-        GlobalTag.create( tag ).fetch().exec(function(err, tag) {
-          if (err) {
-            if (err.code == 'E_UNIQUE') {
-              return mapCb(null, { error: 'Failed Validation'});
-            } else {
-              return mapCb(err);
-            }
-          }
-          return mapCb(null, tag)
-        });
-      }, function(err, tags) {
-        if (err) {
-          return sails.log.error(err);
-        }
-        var cleanedTags = [];
-        tags.map(function(tag) {
-          if (!tag.error) {
-            cleanedTags.push({
-              groupId: tag.groupId,
-              documentKind: tag.documentKind,
-              documentTypeOfService: tag.documentTypeOfService,
-              documentSetting: tag.documentSetting,
-              documentSubjectMatterDomain: tag.documentSubjectMatterDomain,
-              normDSMD: tag.normDSMD,
-              documentRole: tag.documentRole,
-              isDeleted: tag.isDeleted
+          fs.unlinkSync(uploadedFiles[0].fd);
+
+          var tagsArr = [];
+          _.forEach(tagLookUp, function(value, key) {
+            value['origin'] = 'Global';
+            tagsArr.push(value);
+          });
+
+          async.map(tagsArr, function(tag, mapCb) {
+            GlobalTag.create( tag ).fetch().exec(function(err, tag) {
+              if (err) {
+                if (err.code == 'E_UNIQUE') {
+                  return mapCb(null, { error: 'Failed Validation'});
+                } else {
+                  return mapCb(err);
+                }
+              }
+              return mapCb(null, tag)
             });
-          }
-        });
-
-        List.find().exec(function(err, lists) {
-          if (err) {
-            sails.log.error(err);
-            return res.send(500);
-          }
-          sails.helpers.createGlobalTagsForEachList( { lists: lists, tags: cleanedTags } ).switch({
-            error: function(err) { return res.serverError(err); },
-            success: function(suc) {
-              return res.ok();
+          }, function(err, tags) {
+            if (err) {
+              return sails.log.error(err);
             }
+            var cleanedTags = [];
+            tags.map(function(tag) {
+              if (!tag.error) {
+                cleanedTags.push({
+                  groupId: tag.groupId,
+                  documentKind: tag.documentKind,
+                  documentTypeOfService: tag.documentTypeOfService,
+                  documentSetting: tag.documentSetting,
+                  documentSubjectMatterDomain: tag.documentSubjectMatterDomain,
+                  normDSMD: tag.normDSMD,
+                  documentRole: tag.documentRole,
+                  isDeleted: tag.isDeleted
+                });
+              }
+            });
+
+            List.find().exec(function(err, lists) {
+              if (err) {
+                sails.log.error(err);
+                return res.sendStatus(500);
+              }
+              sails.helpers.createGlobalTagsForEachList( { lists: lists, tags: cleanedTags } ).switch({
+                error: function(err) { return res.serverError(err); },
+                success: function(suc) {
+                  return res.send(200, { numCreatedGlobalTags: cleanedTags.length } );
+                }
+              });
+            });
           });
         });
-      });
     });
   },
 
